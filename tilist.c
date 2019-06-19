61dbaad2bd023ea1e65de4b3dcfc2559cb2c4738
Erich Waidthaler <ewaidthaler@kyrillia.com>
2019-06-19 13:52:13 -0500

    Added lodepng

--- tilist.c
+++ tilist.c
@@ -8,12 +8,19 @@
 #include "tilist.h"
 
 struct {               // global config structure
-    Dynarray dir;          // direction array
-    Dynarray vert;         // vertex array
-    Dynarray tile;         // tile master array
+    Dynarray  dir;          // direction array
+    Dynarray  vert;         // vertex array
+    Dynarray  tile;         // tile master array
+    uint16_t  image_width;
+    uint16_t  image_height;
+    Pixel     bgcolor;
+    char     *output_png_name;
 } config;
 
 
+//==============================================================================
+// Main loop.
+//==============================================================================
 
 int main(int argc, char **argv) {
     pre_init();
@@ -21,12 +28,87 @@ int main(int argc, char **argv) {
 }
 
 
+//==============================================================================
+// Initial setup of data structures.
+//==============================================================================
+
 int pre_init() {
     config.dir.nmemb = 32;
     if(!dynarray_create(&config.dir)) {
         printf("Unable to initialize config.dir.\n");
         abort();
     }
+    config.vert.nmemb = 256;
+    if(!dynarray_create(&config.vert)) {
+        printf("Unable to initialize config.vert.\n");
+        abort();
+    }
+    config.tile.nmemb = 256;
+    if(!dynarray_create(&config.tile)) {
+        printf("Unable to initialize config.tile.\n");
+        abort();
+    }
+
+    // Defaults
+
+    config.image_width  = 800;
+    config.image_height = 600;
+
+    config.bgcolor.r    = 0;
+    config.bgcolor.g    = 0;
+    config.bgcolor.b    = 0;
+    config.bgcolor.a    = 0;
+
+    return 0;
 }
 
 
+//==============================================================================
+// Returns a boolean indicating whether the two strings supplied are identical.
+//==============================================================================
+
+bool streq(char *a, char *b) {
+    return strcmp(a, b) ? false : true;
+}
+
+
+//==============================================================================
+// Returns a boolean indicating whether the two strings supplied are identical
+// up to the first n bytes. If either string terminates before the nth
+// character, the result is false.
+//==============================================================================
+
+bool streqn(char *a, char *b, int n) {
+    int i;
+
+    for(i = 0; i < n; i++) {
+        if(a[i] == '\0' || b[i] == '\0' || a[i] != b[i])
+            return false;
+    }
+    return true;
+}
+
+
+//==============================================================================
+//==============================================================================
+
+int load_definitions(char *filename) {
+
+}
+
+
+
+//==============================================================================
+//==============================================================================
+
+
+/*
+
+@IMAGE: h, w, bgcolor, bgimage
+@DIRECTIONS: N, E, S, W     // which translates to 1, 2, 3, 4 (zero is reserved)
+@TILE: [...]
+@VERTEX: order, x, y, N:n1;E:n2;W:n3;S:n4, tiletype:orientation
+
+
+
+*/
--- tilist.h
+++ tilist.h
@@ -6,10 +6,17 @@
 
 #include "dynarray.h"
 
-typedef struct Vertex {  // Definition of lattice vertices
+typedef struct {    // Generic RGBA pixel type
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+    uint8_t a;
+} Pixel;
+
+typedef struct {         // Definition of lattice vertices
     int order;               // order in which vertices are visited
     int *eligible;           // array of eligible tiles or NULL for all
-    int *neighbor;           // array of neighbors by direction
+    int *neighbor;           // array of neighbors by direction, offset matches config.dir.ary
     int tile;                // index of tile master
     int orientation;         // index of orientation
     int x_offset;            // x coord in rendered graphic
@@ -17,28 +24,35 @@ typedef struct Vertex {  // Definition of lattice vertices
 } Vertex;
 
 typedef struct {             // Definition of mating surface/side
-    bool      orientation;       // offset into config.dir.ary
+    bool      direction;         // offset into config.dir.ary
     bool      match_any;         // if true, matches any tile
     bool      endcap;            // if true, can match no tile
     bool      match_label;       // if true, matches only label
     bool      match_labels;      // if true, matches any label in labels
     bool      match_any_of;      // if true, matches any bit in any_of
     bool      match_all_of;      // if true, matches all bits in all_of
-    bool      match_none_of;     // if true, cannot match anyt bit in none_of
-    uint32_t  label;
-    uint32_t *labels;
-    uint32_t  any_of;
-    uint32_t  all_of;
-    uint32_t  none_of;
+    bool      match_none_of;     // if true, cannot match any bit in none_of
+    uint32_t  label;             // if non-zero, matches this label integer
+    uint32_t *labels;            // if not NULL, a null-terminated array of matching labels
+    uint32_t  any_of;            // if match_any_of is true, will match any 1 bit in this bitmask
+    uint32_t  all_of;            // if match_all_of is true, must match all 1 bits in this bitmask
+    uint32_t  none_of;           // if match_none_of is true, must not match any 1 bit in this bitmask
 } Surface;
 
 typedef struct {         // Definition of tile masters
     char *name;              // human-readable tile name
-    Surface *side;           // pointer to array of sides
+    Surface *side;           // pointer to array of sides, offsets matching config.dir.ary
+    char *filename;          // path to sprite sheet
+    int width;               // width of sprite sheet
+    int height;              // height of sprite sheet
+    // TODO: bitmap
+    int x_offset;            // x coord of tile center
+    int y_offset;            // y coord of tile center
 } Tile;
 
 
-int pre_init();
-
+int  pre_init();
+bool streq(char *a, char *b);
+bool streqn(char *a, char *b, int n);
 
 #endif // TILIST_H
